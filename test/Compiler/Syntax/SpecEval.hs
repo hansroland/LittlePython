0490575a315037778fdcc1be47754c3157705e82
Roland Senn <rsx@bluewin.ch>
2025-12-26 09:23:53 +0100

    Improve evaluation test in SpecEval

--- test/Compiler/Syntax/SpecEval.hs
+++ test/Compiler/Syntax/SpecEval.hs
@@ -5,52 +5,48 @@ import Utils.Redir
 import Compiler.Syntax
 import Compiler.Syntax.SampleProgs
 
--- To redirect stdin see:
--- https://stackoverflow.com/questions/76683594/in-haskell-how-can-i-interact-with-stdin-of-an-io
 
-import System.IO.Silently
+evalProgWith :: SProg -> String -> String -> IO ()  --includes get_int
+evalProgWith sprog inp res = do 
+    out <- runWithInput (evalSProg sprog) inp
+    shouldContain out res
 
--- Run the test in the IO Monad
-evalstmt :: SStmt -> IO String
-evalstmt e = do
-    output <- capture_ $ evalSProg $ SProg [e]
-    pure output
-
-evalprog :: SProg -> IO String 
-evalprog p = do 
-   output <- capture_ $ evalSProg p
-   pure output
+evalStmt :: SStmt -> String  -> IO ()           -- without get_int
+evalStmt stmt out = do
+    evalProgWith (SProg [stmt]) "" out
 
 specEval :: Spec
 specEval = do
   describe "Tests for module LangInt.Eval" $ do
-    it "evalstmt testLit01" $ do
-        evalstmt (testLit01) `shouldReturn` "34\n"
-    it "evalstmt testNeg01" $ do
-        evalstmt (testNeg01) `shouldReturn` "-42\n"
-    it "evalstmt testNeg02" $ do
-        evalstmt (testNeg02) `shouldReturn` "5\n"
-    it "evalstmt testAdd01" $ do
-        evalstmt (testAdd01) `shouldReturn` "42\n"
-    it "evalstmt testAdd02" $ do
-        evalstmt (testAdd02) `shouldReturn` "10\n"
-    it "evalstmt testSub01" $ do
-        evalstmt (testSub01) `shouldReturn` "-26\n"
-    it "evalstmt testSub02" $ do
-        evalstmt (testSub02) `shouldReturn` "-2\n"
+    it "evalStmt testLit01" $ do
+        evalStmt testLit01 "34\n"
+    it "evalStmt testNeg01" $ do
+        evalStmt (testNeg01) "-42\n"
+    it "evalStmt testNeg02" $ do
+        evalStmt (testNeg02) "5\n"
+    it "evalStmt testAdd01" $ do
+        evalStmt (testAdd01) "42\n"
+    it "evalStmt testAdd02" $ do
+        evalStmt (testAdd02) "10\n"
+    it "evalStmt testSub01" $ do
+        evalStmt (testSub01) "-26\n"
+    it "evalStmt testSub02" $ do
+        evalStmt (testSub02) "-2\n"
 
-    it "evalprog prog01" $ do
-        evalprog prog01  `shouldReturn` "32\n"
-    it "evalprog prog02" $ do
-        evalprog prog02  `shouldReturn` "42\n"
-    it "evalprog prog03" $ do
-        evalprog prog03  `shouldReturn` "-20\n"
-    it "evalprog prog04" $ do
-        evalprog prog04  `shouldReturn` "-25\n"
+    it "evalProgWith prog01" $ do
+        evalProgWith prog01 "" "32\n"
 
-    it "runWithInput (evalSProg prog05)" $ do 
-        out <- runWithInput (evalSProg prog05) "12\n13"
-        shouldContain out ("-25\n" )
+    it "evalSProg prog02" $ do
+        evalProgWith prog02 "" "42\n"
+
+    it "evalSProg prog03" $ do
+        evalProgWith prog03 "" "-20\n"
+
+    it "evalSProg prog04" $ do
+        evalProgWith prog04 "" "-25\n"
+
+    it "runWievalSProgthInput prog05" $ do 
+        evalProgWith prog05 "12\n13" "-25\n" 
 
 testLit01 :: SStmt
 testLit01 = SStmtCall "print"  (SExprInt 34)
@@ -72,6 +68,3 @@ testSub01 = SStmtCall "print"   (SExprBinOp Sub (SExprInt 8) (SExprInt 34))
 
 testSub02 :: SStmt
 testSub02 = SStmtCall "print"  (SExprBinOp Add (SExprBinOp Sub (SExprInt 1) (SExprInt 2)) (SExprBinOp Sub (SExprInt 3) (SExprInt 4)))
-
-
-
